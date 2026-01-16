c6474346429d814cc37562efc02b1b93e5922609
Daniel Starnes <dstarnes261@gmail.com>
2026-01-16 15:13:01 -0600

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Daniel Starnes on 1/16/26.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
